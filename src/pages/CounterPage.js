0f2d3d57cfd9d6b3135b76ece0ef6926dc0a30a2
nirdesh1234 <35981417+nirdesh1234@users.noreply.github.com>
2024-04-12 12:44:09 +0545

    made use of immer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,44 +1,37 @@
-import { useReducer } from 'react';
-import Button from '../components/Button';
-import Panel from '../components/Panel';
+import { useReducer } from "react";
+import Button from "../components/Button";
+import Panel from "../components/Panel";
+import { produce } from "immer";
 
-const INCREMENT_COUNT = 'increment';
-const SET_VALUE_TO_ADD = 'change_value_to_add';
-const DECREMENT_COUNT = 'decrement';
-const ADD_VALUE_TO_COUNT = 'add_value_to_count';
+const INCREMENT_COUNT = "increment";
+const SET_VALUE_TO_ADD = "change_value_to_add";
+const DECREMENT_COUNT = "decrement";
+const ADD_VALUE_TO_COUNT = "add_value_to_count";
 
-const reducer = (state, action) => {
+const reducer = produce((state, action) => {
   switch (action.type) {
     case INCREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count + 1,
-      };
+      state.count += 1;
+      break;
     case DECREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count - 1,
-      };
+      state.count -= 1;
+      break;
     case ADD_VALUE_TO_COUNT:
-      return {
-        ...state,
-        count: state.count + state.valueToAdd,
-        valueToAdd: 0,
-      };
+      state.count += state.valueToAdd;
+      state.valueToAdd = 0;
+      break;
     case SET_VALUE_TO_ADD:
-      return {
-        ...state,
-        valueToAdd: action.payload,
-      };
+      state.valueToAdd = action.payload;
+      break;
     default:
-      return state;
+      break;
   }
-};
+});
 
 function CounterPage({ initialCount }) {
   // const [count, setCount] = useState(initialCount);
   // const [valueToAdd, setValueToAdd] = useState(0);
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valueToAdd: 0,
   });
@@ -81,7 +74,7 @@ function CounterPage({ initialCount }) {
       <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <input
-          value={state.valueToAdd || ''}
+          value={state.valueToAdd || ""}
           onChange={handleChange}
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-300"
